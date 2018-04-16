6bfee33f5e18759d80f01c2116ad99bf37631a07
zhx <1097195326@qq.com>
2018-04-16 22:21:44 +0800

    first

---
+++ LearnOpenGL/LearnOpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearnOpenGL
+//
+//  Created by hongxing zhang on 2018/4/16.
+//  Copyright © 2018年 hongxing zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
