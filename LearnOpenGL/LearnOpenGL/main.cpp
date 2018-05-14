99e71fc7a956ec32814fca9110bf606ffeea7aa8
zhx <1097195326@qq.com>
2018-05-14 22:34:50 +0800

    5_14

--- LearnOpenGL/LearnOpenGL/main.cpp
+++ LearnOpenGL/LearnOpenGL/main.cpp
@@ -11,6 +11,9 @@
 #include "ShaderProgram.h"
 #include <GLFW/glfw3.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
@@ -41,10 +44,10 @@ int main(int argc, const char * argv[])
     
     //
     float vertices[] = {
-        0.5f,  0.5f, 0.0f, 1.f,0.f,0.f, // top right
-        0.5f, -0.5f, 0.0f, 0.f,1.f,0.f, // bottom right
-        -0.5f, -0.5f, 0.0f,0.f,0.f,1.f,  // bottom left
-        -0.5f,  0.5f, 0.0f,0.f,0.f,0.f   // top left
+        0.5f,  0.5f,    0.0f, 1.f,0.f,0.f,  1.f,1.f,// top right
+        0.5f, -0.5f,    0.0f, 0.f,1.f,0.f,  1.f,0.f,// bottom right
+        -0.5f, -0.5f,   0.0f,0.f,0.f,1.f,   0.f,0.f,// bottom left
+        -0.5f,  0.5f,   0.0f,0.f,0.f,0.f,   0.f,1.f// top left
     };
     unsigned int indices[] = {  // note that we start from 0!
         0, 1, 3,  // first Triangle
@@ -62,17 +65,66 @@ int main(int argc, const char * argv[])
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     
+    // load image
+    GLuint texture1,texture2;
+//    glGenTextures(1, &texture1);
+//    glBindTexture(GL_TEXTURE_2D, texture1);
+//
+//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    
+    int image_width,image_height,image_nrchannel;
+//    unsigned char * data = stbi_load("Resource/Image/container.jpg", &image_width, &image_height, &image_nrchannel, 0);
+//    if(data)
+//    {
+//        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image_width, image_height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+//        //glGenerateMipmap(GL_TEXTURE_2D);
+//    }
+//    stbi_image_free(data);
+    
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    
+    
+    unsigned char * data = stbi_load("Resource/Image/awesomeface.png", &image_width, &image_height, &image_nrchannel, 0);
+    if(data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image_width, image_height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        //glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    stbi_image_free(data);
+    
+    
+    
+    // build shader
     ShaderProgram * mShaderProgram = new ShaderProgram("Shaders/VertexShader.strings","Shaders/FragmentShader.strings");
     
     GLuint aPos;
     aPos = glGetAttribLocation(mShaderProgram->GetShaderProgramId(), "aPos");
     glEnableVertexAttribArray(aPos);
-    glVertexAttribPointer(aPos, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GL_FLOAT), (GLvoid*)0);
+    glVertexAttribPointer(aPos, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (GLvoid*)0);
     
     GLuint aColor;
     aColor = glGetAttribLocation(mShaderProgram->GetShaderProgramId(), "aColor");
     glEnableVertexAttribArray(aColor);
-    glVertexAttribPointer(aColor, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GL_FLOAT), (GLvoid*)(3 * sizeof(GL_FLOAT)));
+    glVertexAttribPointer(aColor, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (GLvoid*)(3 * sizeof(GL_FLOAT)));
+    
+    GLuint aCooPos;
+    aCooPos = glGetAttribLocation(mShaderProgram->GetShaderProgramId(), "aCooPos");
+    glEnableVertexAttribArray(aCooPos);
+    glVertexAttribPointer(aCooPos, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (GLvoid*)(6 * sizeof(GL_FLOAT)));
+    
+//    mShaderProgram->UseShader();
+    
+    glUniform1f(glGetUniformLocation(mShaderProgram->GetShaderProgramId(), "texture1"), 0);
+    glUniform1f(glGetUniformLocation(mShaderProgram->GetShaderProgramId(), "texture2"), 1);
     
     while (!glfwWindowShouldClose(window))
     {
@@ -81,6 +133,12 @@ int main(int argc, const char * argv[])
         glClear(GL_COLOR_BUFFER_BIT);
         
         mShaderProgram->UseShader();
+        
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+        
 //        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         
