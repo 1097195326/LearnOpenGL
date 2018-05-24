dc500033f6001551f1cec46e617d9a002039db2c
fafa <fafa@fafadeMac-Pro.local>
2018-05-24 18:11:17 +0800

    add camera

--- LearnOpenGL/LearnOpenGL/main.cpp
+++ LearnOpenGL/LearnOpenGL/main.cpp
@@ -18,12 +18,91 @@
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtc/type_ptr.hpp"
 
+
+glm::vec3 CameraPos(0,0,3);
+glm::vec3 CameraFront(0,0,-1);
+glm::vec3 CameraUp(0,1,0);
+
+float LastFrame = 0.f;
+float DeltaTime = 0.f;
+
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
+    float CurrentFrame = glfwGetTime();
+    DeltaTime = CurrentFrame - LastFrame;
+    LastFrame = CurrentFrame;
+    
+    printf("DeltaTime : %f \n",DeltaTime);
+    float CameraSpeed = 2.5f * DeltaTime;
+    
     if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
     {
         glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }else if(key == GLFW_KEY_W && action == GLFW_PRESS)
+    {
+        CameraPos += CameraSpeed * CameraFront;
+    }else if(key == GLFW_KEY_S && action == GLFW_PRESS)
+    {
+        CameraPos -= CameraSpeed * CameraFront;
+    }else if(key == GLFW_KEY_A && action == GLFW_PRESS)
+    {
+        CameraPos -= glm::normalize(glm::cross(CameraFront, CameraUp)) * CameraSpeed;
+    }else if(key == GLFW_KEY_D && action == GLFW_PRESS)
+    {
+        CameraPos += glm::normalize(glm::cross(CameraFront, CameraUp)) * CameraSpeed;
+    }//else if(key == GLFW_KEY_W && action == GLFW_PRESS)
+    {
+        
     }
+}
+bool firstMouse = true;
+float yaw   = -90.0f;
+float pitch =  0.0f;
+float lastX = 400.f;
+float lastY = 300.f;
+float fov = 45.f;
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+    
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+    lastX = xpos;
+    lastY = ypos;
+    
+    float sensitivity = 0.1f; // change this value to your liking
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+    
+    yaw += xoffset;
+    pitch += yoffset;
+    
+    // make sure that when pitch is out of bounds, screen doesn't get flipped
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+    
+    glm::vec3 front;
+    front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    front.y = sin(glm::radians(pitch));
+    front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    CameraFront = glm::normalize(front);
+}
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    if (fov >= 1.0f && fov <= 45.0f)
+        fov -= yoffset;
+    if (fov <= 1.0f)
+        fov = 1.0f;
+    if (fov >= 45.0f)
+        fov = 45.0f;
 }
 int main(int argc, const char * argv[])
 {
@@ -39,19 +118,67 @@ int main(int argc, const char * argv[])
         glfwTerminate();
     }
     glfwSetKeyCallback(window, &key_callback);
+    glfwSetCursorPosCallback(window, &mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+    
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    
     glfwMakeContextCurrent(window);
     
     int screen_width,screen_height;
     glfwGetFramebufferSize(window, &screen_width, &screen_height);
     glViewport(0, 0, screen_width, screen_height);
     
-    //
     float vertices[] = {
-        0.5f,  0.5f,    0.0f, 1.f,0.f,0.f,  1.f,1.f,// top right
-        0.5f, -0.5f,    0.0f, 0.f,1.f,0.f,  1.f,0.f,// bottom right
-        -0.5f, -0.5f,   0.0f,0.f,0.f,1.f,   0.f,0.f,// bottom left
-        -0.5f,  0.5f,   0.0f,0.f,0.f,0.f,   0.f,1.f// top left
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+        
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
+    //
+//    float vertices[] = {
+//        0.5f,  0.5f,    0.0f, 1.f,0.f,0.f,  1.f,1.f,// top right
+//        0.5f, -0.5f,    0.0f, 0.f,1.f,0.f,  1.f,0.f,// bottom right
+//        -0.5f, -0.5f,   0.0f,0.f,0.f,1.f,   0.f,0.f,// bottom left
+//        -0.5f,  0.5f,   0.0f,0.f,0.f,0.f,   0.f,1.f// top left
+//    };
     unsigned int indices[] = {  // note that we start from 0!
         0, 1, 3,  // first Triangle
         1, 2, 3   // second Triangle
@@ -114,17 +241,18 @@ int main(int argc, const char * argv[])
     GLuint aPos;
     aPos = glGetAttribLocation(mShaderProgram->GetShaderProgramId(), "aPos");
     glEnableVertexAttribArray(aPos);
-    glVertexAttribPointer(aPos, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (GLvoid*)0);
+    glVertexAttribPointer(aPos, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GL_FLOAT), (GLvoid*)0);
     
-    GLuint aColor;
-    aColor = glGetAttribLocation(mShaderProgram->GetShaderProgramId(), "aColor");
-    glEnableVertexAttribArray(aColor);
-    glVertexAttribPointer(aColor, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (GLvoid*)(3 * sizeof(GL_FLOAT)));
+//    GLuint aColor;
+//    aColor = glGetAttribLocation(mShaderProgram->GetShaderProgramId(), "aColor");
+//    glEnableVertexAttribArray(aColor);
+//    glVertexAttribPointer(aColor, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (GLvoid*)(3 * sizeof(GL_FLOAT)));
     
     GLuint aCooPos;
     aCooPos = glGetAttribLocation(mShaderProgram->GetShaderProgramId(), "aCooPos");
     glEnableVertexAttribArray(aCooPos);
-    glVertexAttribPointer(aCooPos, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (GLvoid*)(6 * sizeof(GL_FLOAT)));
+    glVertexAttribPointer(aCooPos, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GL_FLOAT), (GLvoid*)(3 * sizeof(GL_FLOAT)));
+    
     
     mShaderProgram->UseShader();
     
@@ -132,27 +260,29 @@ int main(int argc, const char * argv[])
     glUniform1i(glGetUniformLocation(mShaderProgram->GetShaderProgramId(), "texture2"), 1);
     
     
-    glm::mat4 model(1.f);
-    model = glm::rotate(model, glm::radians(-45.f), glm::vec3(1,0,0));
-    glUniformMatrix4fv(glGetUniformLocation(mShaderProgram->GetShaderProgramId(), "model"), 1, GL_FALSE, glm::value_ptr(model));
-    
-    glm::mat4 view(1.f);
-    view = glm::translate(view, glm::vec3(0,0,-3));
-    glUniformMatrix4fv(glGetUniformLocation(mShaderProgram->GetShaderProgramId(), "view"), 1, GL_FALSE, glm::value_ptr(view));
-    
-    glm::mat4 projection = glm::perspective(glm::radians(45.f), (float)screen_width/(float)screen_height, 0.1f, 100.f);
-    glUniformMatrix4fv(glGetUniformLocation(mShaderProgram->GetShaderProgramId(), "projection"), 1, GL_FALSE, glm::value_ptr(projection));
-    
+    glEnable(GL_DEPTH_TEST);
     
     while (!glfwWindowShouldClose(window))
     {
         // rendering
         glClearColor(0, 1, 0, 1);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
         mShaderProgram->UseShader();
         
+        glm::mat4 model(1.f);
+        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(55.f), glm::vec3(0.5,1,0));
+        glUniformMatrix4fv(glGetUniformLocation(mShaderProgram->GetShaderProgramId(), "model"), 1, GL_FALSE, glm::value_ptr(model));
         
+//        glm::mat4 view(1.f);
+//        view = glm::translate(view, glm::vec3(0,0,-3));
+        
+        glm::mat4 view = glm::lookAt(CameraPos, CameraPos + CameraFront, CameraUp);
+        glUniformMatrix4fv(glGetUniformLocation(mShaderProgram->GetShaderProgramId(), "view"), 1, GL_FALSE, glm::value_ptr(view));
+        
+        
+        glm::mat4 projection = glm::perspective(glm::radians(fov), (float)screen_width/(float)screen_height, 0.1f, 100.f);
+        glUniformMatrix4fv(glGetUniformLocation(mShaderProgram->GetShaderProgramId(), "projection"), 1, GL_FALSE, glm::value_ptr(projection));
         
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture1);
@@ -160,7 +290,8 @@ int main(int argc, const char * argv[])
         glBindTexture(GL_TEXTURE_2D, texture2);
         
 //        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+//        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
         
         glfwSwapBuffers(window);
         glfwPollEvents();
