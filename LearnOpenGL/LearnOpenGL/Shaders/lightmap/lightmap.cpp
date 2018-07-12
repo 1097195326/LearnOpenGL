428fcdb98fa5629cd172109b0cce8034d4a80f9d
1097195326 <1097195326@qq.com>
2018-07-12 15:51:33 +0800

    add lightmap

--- LearnOpenGL/LearnOpenGL/Shaders/lightmap/lightmap.cpp
+++ LearnOpenGL/LearnOpenGL/Shaders/lightmap/lightmap.cpp
@@ -6,18 +6,18 @@
 //  Copyright © 2018年 hongxing zhang. All rights reserved.
 //
 
-#include "material.hpp"
+#include "lightmap.hpp"
 #include "Camera.h"
 #include "GActor.h"
 #include "CameraManager.h"
 
 
-void Material::Draw(GLFWwindow * window)
+void LightMap::Draw(GLFWwindow * window)
 {
     glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
     // camera
     Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
-    
+
     float vertices3[] = {
         -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
         0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
@@ -25,35 +25,35 @@ void Material::Draw(GLFWwindow * window)
         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
         -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
         -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-        
+
         -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
         0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
         -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
         -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-        
+
         -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
         -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
         -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
         -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
         -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
         -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-        
+
         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
         0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
         0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
-        
+
         -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
         0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
         -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
         -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
-        
+
         -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
         0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
@@ -61,10 +61,10 @@ void Material::Draw(GLFWwindow * window)
         -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
         -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
     };
-    
+
     CameraManager::Get()->SetCamera(&camera);
-    
-    
+
+
     GActor * actor = new GActor();
     actor->SetData(vertices3,sizeof(vertices3), 36);
     actor->SetShader("Shaders/material/mvs.strings","Shaders/material/mfs.strings",true ,false, false);
@@ -78,24 +78,24 @@ void Material::Draw(GLFWwindow * window)
     actor->GetShader()->SetUniform3fv("material.diffuse", vec3(1.0f, 0.5f, 0.31f));
     actor->GetShader()->SetUniform3fv("material.specular", vec3(0.5f, 0.5f, 0.5f));
     actor->GetShader()->SetUniform1f("material.shininess", 32.0f);
-    
+
     GActor * lightObject = new GActor();
     lightObject->SetData(vertices3,sizeof(vertices3), 36);
     lightObject->SetShader("Shaders/material/lvs.strings","Shaders/material/lfs.strings",true ,false, false);
     lightObject->SetPosition(lightPos);
     lightObject->GetShader()->SetUniform3fv("LightColor", vec3(1.f));
-    
+
     glEnable(GL_DEPTH_TEST);
-    
+
     while (!glfwWindowShouldClose(window))
     {
         // rendering
         glClearColor(0.0, 0.5, 0.0, 1.0);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        
+
         actor->Draw();
         lightObject->Draw();
-        
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
---
+++ LearnOpenGL/LearnOpenGL/Shaders/lightmap/lightmap.hpp
@@ -0,0 +1,23 @@
+//
+//  material.hpp
+//  LearnOpenGL
+//
+//  Created by fafa on 2018/7/10.
+//  Copyright © 2018年 hongxing zhang. All rights reserved.
+//
+
+#ifndef material_hpp
+#define material_hpp
+
+#include "glad.h"
+#include <GLFW/glfw3.h>
+
+namespace LightMap
+{
+    void  Draw(GLFWwindow * window);
+    
+}
+
+
+
+#endif /* material_hpp */
