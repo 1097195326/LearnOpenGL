1392875e3b9c38fb1321c3b5f1d96718fac31f75
张洪星 <1097195326@qq.com>
2019-09-18 16:51:05 +0800

    new branch

--- LearnOpenGL/LearnOpenGL/GameFrame/Camera/Camera.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Camera.cpp
-//  LearnOpenGL
-//
-//  Created by fafa on 2018/5/30.
-//  Copyright © 2018年 hongxing zhang. All rights reserved.
-//
-
-#include "Camera.h"
--- LearnOpenGL/LearnOpenGL/GameFrame/Managers/ObjectManager.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  ObjectManager.cpp
-//  LearnOpenGL
-//
-//  Created by fafa on 2018/5/25.
-//  Copyright © 2018年 hongxing zhang. All rights reserved.
-//
-
-#include "ObjectManager.h"
